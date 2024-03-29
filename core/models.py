925736176c84bdb3c98b5a582ea7d3a47f222f55
erose-dti <eddy.rose@dtisim.com>
2024-03-29 11:35:23 -0500

    Fixed models migration bug

--- core/models.py
+++ core/models.py
@@ -3,12 +3,12 @@ from django.db import models
 # TODO: Question: Should there be two tables? raw and embedded? 
 class Artifact(models.Model):
     # TODO: Add fields for full_entry
-    title = models.CharField(max_length=50)
-    description = models.TextField()
-    descriptionWC = models.PositiveIntegerField()
-    description_cleaned = models.TextField()
-    description_tokenized = models.TextField()
-    embed_raw = models.FloatField()
+    title = models.CharField(max_length=50, default='None')
+    description = models.TextField(default='None')
+    descriptionWC = models.PositiveIntegerField(default=0)
+    description_cleaned = models.TextField(default='None')
+    description_tokenized = models.TextField(default='None')
+    embed_raw = models.FloatField(default=0.0)
 
     def __str__(self):
         return str(self.id)
